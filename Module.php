20d7b6cfd2d8c03f966a0d058da8fd8fe34a2c6b
Payam Naderi <naderi.payam@gmail.com>
2013-12-19 17:54:37 +0330

    Corect module structure for composer install and also available with git clone

--- Module.php
+++ Module.php
@@ -2,6 +2,14 @@
 /**
  * This file is placed here for compatibility with Zendframework 2's ModuleManager.
  * It allows usage of this module even without composer.
- * The original Module.php is in 'src/Skeleton' in order to respect PSR-0
+ * The original Module.php is in 'src/ySkeletonModule' in order to respect PSR-0
+ *
+ * if we use composer :
+ * "autoload" : {
+        "psr-0" : {
+            "ySkeletonModule\\" : "src/"
+    },
+ *
+ *  the autoloader never fallback into \Zend\Loader\ModuleAutoloader
  */
-require_once __DIR__ . '/src/Skeleton/Module.php';
+require_once __DIR__ . '/src/ySkeletonModule/Module.php';
